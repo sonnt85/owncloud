9f2916929d5648036e486ca778602ee58ecb3bb0
sonnt85 <nts2201@gmail.com>
2016-12-25 12:50:12 +0700

    Update docker-entrypoint.sh
    shutdown all services when has shutdown container

--- 9.1/apache/docker-entrypoint.sh
+++ 9.1/apache/docker-entrypoint.sh
@@ -7,4 +7,16 @@ if [ ! -e '/var/www/html/owncloud/version.php' ]; then
 fi
 service redis-server start
 service ssh start
+service apache2 start
+function sigterm_handler() {
+        echo "SIGTERM signal received, try to gracefully shutdown all services..."
+        service mysql stop
+	service redis-server stop 
+	service apache2 stop
+	service memcached stop
+	exit 0
+}
+
+trap "sigterm_handler; exit" TERM
+while((1));do sleep 10; done;
 exec "$@"
