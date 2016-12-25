f3f0b0a6676a0118d54cce17aab378ec49763ac3
sonnt85 <nts2201@gmail.com>
2016-12-25 09:44:49 +0700

    Update docker-entrypoint.sh

--- 9.1/apache/docker-entrypoint.sh
+++ 9.1/apache/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ ! -e '/var/www/html/version.php' ]; then
+if [ ! -e '/var/www/html/owncloud/version.php' ]; then
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
-	chown -R www-data /var/www/html
+	chown -R www-data /var/www/html/owncloud
 fi
-
+service redis-server start
+service ssh start
 exec "$@"
